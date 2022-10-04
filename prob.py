412e60db934f0672b76d688e6c7ffa0bdedd21bb
Mauricio-A-Monroy <alejomonry@hotmail.com>
2022-10-04 12:59:49 -0500

    Commit

--- prob.py
+++ prob.py
@@ -12,18 +12,35 @@ def prob(v,p):
         mod=mod+(abs(aux[i]))**2
     print((abs(aux[p])**2)/mod)
 
+#Función para normalizar un vector
+def normalizar_vec(v):
+    norma=numpy.linalg.norm(v)
+    for i in range(len(v)):
+        v[i]=v[i]/norma
+    return v
+
+#Función que calcula la amplitud de transición
+def amp_tran(v1,v2):
+    """"""
+    v1=normalizar_vec(v1)
+    v2 = normalizar_vec(v2)
+    for i in range(len(v1)):
+        v1[i]=v1[i].conjugate()
+    prod_inter=numpy.inner(v1,v2)
+    return prod_inter
+
 #Función para buscar la probabilidad de transitar del primer vector al segundo.
 def prob_trans(v1,v2):
-    """"""
-    v1_norm=numpy.linalg.norm(v1)
-    for i in range(len(v1)):
-        v1[i]=v1[i]/v1_norm
-    v2_norm=numpy.linalg.norm(v2)
-    for i in range(len(v2)):
-        v2[i]=v2[i]/v2_norm
-    prod_inter=numpy.inner(v1,v2)
-    print(prod_inter)
-prob_trans([1,-1j],[1j,1])
+    return abs(amp_tran(v1,v2))**2
+
+amp_tran([1j, 1], [1, -1j])
+prob_trans([1j, 1], [1, -1j])
+
+#Función que calcula la media y la varianza del observable en el estado dado.
+def media_varianza(m,v):
+    new_m=numpy.matrix.H
+    if m!=new_m:
+        raise Exception("La matriz no es hermitiana")
 
 
 
